b30cc4b2787a9e07f8f6dd2e4f3e3fca79a14b66
RaphaLima70 <samurai_raphael@hotmail.com>
2020-07-12 21:58:55 -0300

    Upando projeto

---
+++ Assets/scripts/scr_drag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_drag : MonoBehaviour {
+
+    Vector3 ponteiro;
+
+    public void OnMouseDrag()
+    {
+        ponteiro = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ponteiro.y = transform.position.y;
+        ponteiro.z = transform.position.z;
+        transform.position = ponteiro;
+    }
+
+}
